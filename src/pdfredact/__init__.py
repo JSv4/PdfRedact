0bcc5c7e02f13857c7a727e17d7a8844e07830ec
JSv4 <scrudato@umich.edu>
2025-01-09 23:15:15 -0600

    Initial working setup.

--- src/pdfredact/__init__.py
+++ src/pdfredact/__init__.py
@@ -1,3 +1,12 @@
 # SPDX-FileCopyrightText: 2025-present JSv4 <scrudato@umich.edu>
 #
 # SPDX-License-Identifier: MIT
+from .utils import (
+    redact_pdf_to_images,
+    build_text_redacted_pdf,
+)
+
+__all__ = [
+    "redact_pdf_to_images",
+    "build_text_redacted_pdf",
+]
